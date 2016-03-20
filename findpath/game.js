548425c4dd09f4e8cec20c9601c9cc3b414ec6d1
XiaJingHong <2364731228@qq.com>
2016-03-20 08:56:49 +0800

    1

--- findpath/game.js
+++ findpath/game.js
@@ -61,28 +61,49 @@ var game;
         __extends(BoyBody, _super);
         function BoyBody() {
             _super.apply(this, arguments);
+            this.width = GRID_PIXEL_WIDTH;
+            this.height = GRID_PIXEL_HEIGHT;
+            this.steps = 1;
         }
         BoyBody.prototype.run = function (grid) {
             grid.setStartNode(0, 0);
+            this.x = grid.startNode.x * this.width;
+            this.y = grid.startNode.y * this.height;
             grid.setEndNode(10, 8);
             var findpath = new astar.AStar();
             findpath.setHeurisitic(findpath.diagonal);
             var result = findpath.findPath(grid);
             var path = findpath._path;
-            console.log(path);
+            this.path = path;
+            console.log(this.path);
             console.log(grid.toString());
         };
         BoyBody.prototype.onTicker = function (duringTime) {
+            if (this.steps < this.path.length - 1) {
+                var targetx = this.path[this.steps].x * this.width;
+                var targety = this.path[this.steps].y * this.height;
+                if (this.x < targetx) {
+                    this.x = (this.x + this.vx * duringTime > targetx) ? targetx : (this.x + this.vx * duringTime);
+                }
+                if (this.y < targety) {
+                    this.y = (this.y + this.vy * duringTime > targety) ? targety : (this.y + this.vy * duringTime);
+                }
+                if (this.x == targetx && this.y == targety) {
+                    this.steps += 1;
+                }
+            }
+            console.log(this.x, this.y, this.steps);
         };
         return BoyBody;
     }(Body));
     game.BoyBody = BoyBody;
+    var boyShape = new game.BoyShape();
+    var world = new game.WorldMap();
+    var body = new game.BoyBody(boyShape);
+    body.run(world.grid);
+    var renderCore = new RenderCore();
+    renderCore.start([world, boyShape]);
+    var ticker = new Ticker();
+    ticker.start([body]);
+    ticker.onTicker();
 })(game || (game = {}));
-var boyShape = new game.BoyShape();
-var world = new game.WorldMap();
-var body = new game.BoyBody(boyShape);
-body.run(world.grid);
-var renderCore = new RenderCore();
-renderCore.start([world, boyShape]);
-var ticker = new Ticker();
-ticker.start([body]);
--- findpath/game.ts
+++ findpath/game.ts
@@ -59,23 +59,42 @@ module game {
     }
 
     export class BoyBody extends Body {
+        width=GRID_PIXEL_WIDTH;
+        height=GRID_PIXEL_HEIGHT;
+        steps=1;
+        path;
 
 
         public run(grid) {
             grid.setStartNode(0, 0);
+            this.x=grid.startNode.x*this.width; 
+            this.y=grid.startNode.y*this.height; 
             grid.setEndNode(10, 8);
             var findpath = new astar.AStar();
             findpath.setHeurisitic(findpath.diagonal);
             var result = findpath.findPath(grid);
             var path = findpath._path;
-            console.log(path);
+            this.path=path;
+            console.log(this.path);
             console.log(grid.toString());
         }
 
         public onTicker(duringTime) {
-
+            if (this.steps < this.path.length - 1) {
+                var targetx = this.path[this.steps].x * this.width;
+                var targety = this.path[this.steps].y * this.height;
+                if (this.x < targetx) {
+                    this.x = (this.x + this.vx * duringTime > targetx) ? targetx : (this.x + this.vx * duringTime);
+                }
+                if (this.y < targety) {
+                    this.y = (this.y + this.vy * duringTime > targety) ? targety : (this.y + this.vy * duringTime);
+                }
+                if (this.x == targetx && this.y == targety) {
+                    this.steps += 1;
+                }
+            }
+            console.log(this.x,this.y,this.steps);
         }
-    }
 }
 
 
@@ -92,3 +111,5 @@ renderCore.start([world, boyShape]);
 
 var ticker = new Ticker();
 ticker.start([body]);
+ticker.onTicker(); 
+}
